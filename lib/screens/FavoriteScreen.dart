faafaf0d9b846fc0c0ec3e225716c6879ebb4997
Wicharn Rueangkhajorn <wicharn.keisei@gmail.com>
2020-04-28 00:57:37 +0700

    [UPDATE]favorite work properly but don't bully me for bad code

--- lib/screens/FavoriteScreen.dart
+++ lib/screens/FavoriteScreen.dart
@@ -28,113 +28,132 @@ class _FavoriteScreenState extends State<FavoriteScreen> {
   Padding _buildNormalActivityList() {
     return Padding(
         padding: const EdgeInsets.all(16.0),
-        child: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              StreamBuilder<QuerySnapshot>(
-                  stream:
-                      Firestore.instance.collection('news').snapshots(),
-                  builder: (BuildContext context,
-                      AsyncSnapshot<QuerySnapshot> snapshot) {
-                    if (snapshot.hasError)
-                      return new Text('Error: ${snapshot.error}');
-                    switch (snapshot.connectionState) {
-                      case ConnectionState.waiting:
-                        return new Center(
-                          child: CircularProgressIndicator(),
-                        );
-                      default:
-                        return new ListView(
-                            scrollDirection: Axis.vertical,
-                            shrinkWrap: true,
-                            children: snapshot.data.documents
-                                .where((DocumentSnapshot doc) {
-                              return _storage.getItem(doc.documentID) != null
-                                  ? _storage.getItem(doc.documentID) == 'true'
-                                  : false;
-                            }).map((DocumentSnapshot doc) {
-                              return Hero(
-                                tag: doc.documentID.toString(),
-                                child: ActivityCard(
-                                  id: doc.documentID,
-                                  title: doc['title'],
-                                  imageUrl: doc['imageUrl'],
-                                  category: 'competition',
-                                ),
-                              );
-                            }).toList());
-                    }
-                  }),
-              StreamBuilder<QuerySnapshot>(
-                  stream: Firestore.instance.collection('activity').snapshots(),
-                  builder: (BuildContext context,
-                      AsyncSnapshot<QuerySnapshot> snapshot) {
-                    if (snapshot.hasError)
-                      return new Text('Error: ${snapshot.error}');
-                    switch (snapshot.connectionState) {
-                      case ConnectionState.waiting:
-                        return new Center(
-                          child: CircularProgressIndicator(),
-                        );
-                      default:
-                        return new ListView(
-                            scrollDirection: Axis.vertical,
-                            shrinkWrap: true,
-                            children: snapshot.data.documents
-                                .where((DocumentSnapshot doc) {
-                              return _storage.getItem(doc.documentID) != null
-                                  ? _storage.getItem(doc.documentID) == 'true'
-                                  : false;
-                            }).map((DocumentSnapshot doc) {
-                              return Hero(
-                                tag: doc.documentID.toString(),
-                                child: ActivityCard(
-                                  id: doc.documentID,
-                                  title: doc['title'],
-                                  imageUrl: doc['imageUrl'],
-                                  category: 'activity',
-                                ),
-                              );
-                            }).toList());
-                    }
-                  }),
-              StreamBuilder<QuerySnapshot>(
-                  stream:
-                      Firestore.instance.collection('competition').snapshots(),
-                  builder: (BuildContext context,
-                      AsyncSnapshot<QuerySnapshot> snapshot) {
-                    if (snapshot.hasError)
-                      return new Text('Error: ${snapshot.error}');
-                    switch (snapshot.connectionState) {
-                      case ConnectionState.waiting:
-                        return new Center(
-                          child: CircularProgressIndicator(),
-                        );
-                      default:
-                        return new ListView(
-                            scrollDirection: Axis.vertical,
-                            shrinkWrap: true,
-                            children: snapshot.data.documents
-                                .where((DocumentSnapshot doc) {
-                              return _storage.getItem(doc.documentID) != null
-                                  ? _storage.getItem(doc.documentID) == 'true'
-                                  : false;
-                            }).map((DocumentSnapshot doc) {
-                              return Hero(
-                                tag: doc.documentID.toString(),
-                                child: ActivityCard(
-                                  id: doc.documentID,
-                                  title: doc['title'],
-                                  imageUrl: doc['imageUrl'],
-                                  category: 'competition',
-                                ),
-                              );
-                            }).toList());
-                    }
-                  }),
-            ],
-          ),
-        ));
+        child: StreamBuilder<QuerySnapshot>(
+            stream: Firestore.instance.collection('news').snapshots(),
+            builder: (BuildContext context,
+                AsyncSnapshot<QuerySnapshot> snapshot) {
+              if (snapshot.hasError)
+                return new Text('Error: ${snapshot.error}');
+              switch (snapshot.connectionState) {
+                case ConnectionState.waiting:
+                  return new Center(
+                    child: CircularProgressIndicator(),
+                  );
+                default:
+                  return StreamBuilder<QuerySnapshot>(
+                      stream: Firestore.instance
+                          .collection('activity')
+                          .snapshots(),
+                      builder: (BuildContext context,
+                          AsyncSnapshot<QuerySnapshot> snapshot1) {
+                        if (snapshot1.hasError)
+                          return new Text('Error: ${snapshot1.error}');
+                        switch (snapshot1.connectionState) {
+                          case ConnectionState.waiting:
+                            return new Center(
+                              child: CircularProgressIndicator(),
+                            );
+                          default:
+                            return StreamBuilder<QuerySnapshot>(
+                                stream: Firestore.instance
+                                    .collection('competition')
+                                    .snapshots(),
+                                builder: (BuildContext context,
+                                    AsyncSnapshot<QuerySnapshot>
+                                        snapshot2) {
+                                  if (snapshot2.hasError)
+                                    return new Text(
+                                        'Error: ${snapshot2.error}');
+                                  switch (snapshot2.connectionState) {
+                                    case ConnectionState.waiting:
+                                      return new Center(
+                                        child:
+                                            CircularProgressIndicator(),
+                                      );
+                                    default:
+                                      return new ListView(
+                                          scrollDirection: Axis.vertical,
+                                          shrinkWrap: true,
+                                          children: [
+                                            ...snapshot.data.documents
+                                                .where((DocumentSnapshot
+                                                    doc) {
+                                              return _storage.getItem(doc
+                                                          .documentID) !=
+                                                      null
+                                                  ? _storage.getItem(doc
+                                                          .documentID) ==
+                                                      'true'
+                                                  : false;
+                                            }).map((DocumentSnapshot
+                                                    doc) {
+                                              return Hero(
+                                                tag: doc.documentID
+                                                    .toString(),
+                                                child: ActivityCard(
+                                                  id: doc.documentID,
+                                                  title: doc['title'],
+                                                  imageUrl:
+                                                      doc['imageUrl'],
+                                                  category: 'news',
+                                                ),
+                                              );
+                                            }).toList(),
+                                            ...snapshot1.data.documents
+                                                .where((DocumentSnapshot
+                                                    doc) {
+                                              return _storage.getItem(doc
+                                                          .documentID) !=
+                                                      null
+                                                  ? _storage.getItem(doc
+                                                          .documentID) ==
+                                                      'true'
+                                                  : false;
+                                            }).map((DocumentSnapshot
+                                                    doc) {
+                                              return Hero(
+                                                tag: doc.documentID
+                                                    .toString(),
+                                                child: ActivityCard(
+                                                  id: doc.documentID,
+                                                  title: doc['title'],
+                                                  imageUrl:
+                                                      doc['imageUrl'],
+                                                  category: 'activity',
+                                                ),
+                                              );
+                                            }).toList(),
+                                            ...snapshot2.data.documents
+                                                .where((DocumentSnapshot
+                                                    doc) {
+                                              return _storage.getItem(doc
+                                                          .documentID) !=
+                                                      null
+                                                  ? _storage.getItem(doc
+                                                          .documentID) ==
+                                                      'true'
+                                                  : false;
+                                            }).map((DocumentSnapshot
+                                                    doc) {
+                                              return Hero(
+                                                tag: doc.documentID
+                                                    .toString(),
+                                                child: ActivityCard(
+                                                  id: doc.documentID,
+                                                  title: doc['title'],
+                                                  imageUrl:
+                                                      doc['imageUrl'],
+                                                  category: 'competition',
+                                                ),
+                                              );
+                                            }).toList()
+                                          ]);
+                                  }
+                                });
+                        }
+                      });
+              }
+            }));
   }
 }
 //ListView(
