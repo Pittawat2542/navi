389a7435f8de97992de925e4e482119a4521240c
Wicharn Rueangkhajorn <wicharn.keisei@gmail.com>
2020-05-04 11:42:15 +0700

    [update] version

--- lib/screens/SettingsScreen.dart
+++ lib/screens/SettingsScreen.dart
@@ -10,7 +10,7 @@ class SettingsScreen extends StatelessWidget {
       body: Center(
         child: Container(
           color: Colors.white,
-          child: Text("Version 1.1.4")
+          child: Text("Version 1.1.5")
         ),
       ),
     );
